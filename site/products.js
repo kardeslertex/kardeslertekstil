0e46606baf2e79a8e132709ddb7c620f3cd07a4a
kardeslertex <rizacanari34@gmail.com>
2026-07-23 14:14:57 +0300

    f
    ff

--- site/products.js
+++ site/products.js
@@ -99,7 +99,7 @@ window.KATALOG = [
     unit: "model",
     urunler: [
       "01.jpeg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg",
-      "09.jpg", "10.jpg", "11.jpeg", "12.jpeg", "13.jpeg", "14.jpeg", "15.jpeg", "16.jpg", "17.png", "16.png"
+      "09.jpg", "10.jpg", "11.jpeg", "12.jpeg", "13.jpeg", "14.jpeg", "15.jpeg", "16.jpg", "17.png", "18.png"
     ]
   },
 
