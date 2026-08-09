30c5d0e640715952e20137add953c053e4da9780
kardeslertex <rizacanari34@gmail.com>
2026-08-09 12:19:59 +0300

    Ana sayfa güven ikonlarını görünür hale getir

--- site/_worker.js
+++ site/_worker.js
@@ -29,6 +29,7 @@ const RELEASE_ASSET_ALIASES = new Map([
   ["/products-data-20260809-kt-on-040-white1.js", "/products.js"],
   ["/catalog-ui-20260809-uniformfit3.js", "/catalog.js"],
   ["/catalog-styles-20260809-uniformfit3.css", "/styles.css"],
+  ["/home-styles-20260809-trust-icons3.css", "/styles.css"],
   ["/home-styles-20260809-trust-icons2.css", "/styles.css"],
   ["/home-styles-20260809-trust8.css", "/styles.css"],
   ["/catalog-ui-20260809-technicalpants2.js", "/catalog.js"],
